38ca7ada3a53816693234fbd3b7f38171b7b0aef
zeeshanjavaid568 <zeeshan.568gb@gmail.com>
2024-10-30 17:08:15 +0500

    infinity counter app created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Grid from './components/Grid';
+import DoubleButtonGrid from './components/DoubleButtonGrid.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Routes>
+        <Route path="/" element={<Grid />} /> 
+        <Route path='/doublebuttoncounter' element={<DoubleButtonGrid />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Grid.jsx
@@ -0,0 +1,136 @@
+import React, { useEffect, useState } from 'react';
+import { Link, NavLink } from "react-router-dom";
+
+const Grid = () => {
+    const [counter1, setCounter1] = useState(1);
+    const [counter2, setCounter2] = useState(2);
+    const [counter3, setCounter3] = useState(3);
+    const [counter4, setCounter4] = useState(4);
+    const [counter5, setCounter5] = useState(5);
+    const [counter6, setCounter6] = useState(6);
+    const [intervalId, setIntervalId] = useState(null);
+    const [isRunning, setIsRunning] = useState(false);
+
+    const handleCounters = () => {
+        setCounter1((prev) => prev + 1);
+        setCounter2((prev) => prev + 1);
+        setCounter3((prev) => prev + 1);
+        setCounter4((prev) => prev + 1);
+        setCounter5((prev) => prev + 1);
+        setCounter6((prev) => prev + 1);
+    };
+
+    useEffect(() => {
+        const id = setInterval(handleCounters, 1000);
+        setIntervalId(id);
+        setIsRunning(true);
+
+        return () => clearInterval(id);
+    }, []);
+
+    const startCounter = () => {
+        const id = setInterval(handleCounters, 1000);
+        setIntervalId(id);
+        setIsRunning(true);
+    };
+
+    const stopCounter = () => {
+        clearInterval(intervalId);
+        setIsRunning(false);
+        console.log('Interval cleared');
+    };
+
+    const toggleCounter = () => {
+        if (isRunning) {
+            stopCounter();
+        } else {
+            startCounter();
+        }
+    };
+
+    const resetCounters = () => {
+        setCounter1(1);
+        setCounter2(2);
+        setCounter3(3);
+        setCounter4(4);
+        setCounter5(5);
+        setCounter6(6);
+        stopCounter(); 
+    };
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                fontSize: '300%',
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+                top: '-15%',
+            }}
+        >
+            <Link to={'/doublebuttoncounter'} style={{ textDecoration: 'none', marginBottom: '30px' }}>
+                Double Button Counter App
+            </Link>
+            Counter App
+            <div
+                style={{
+                    fontSize: '120%',
+                    position: 'relative',
+                    top: '10vh',
+                    width: '300px',
+                }}
+            >
+                <div className='row'>
+                    <div className='col-4'>{counter1}</div>
+                    <div className='col-4'>{counter2}</div>
+                    <div className='col-4'>{counter3}</div>
+                </div>
+                <div className='row'>
+                    <div className='col-4'>{counter4}</div>
+                    <div className='col-4'>{counter5}</div>
+                    <div className='col-4'>{counter6}</div>
+                </div>
+            </div>
+            <div className="buttons">
+                <button
+                    style={{
+                        fontSize: '60%',
+                        position: 'relative',
+                        top: '20vh',
+                        marginRight: '5px',
+                        backgroundColor: isRunning ? '#ffd32c' : 'green',
+                        borderRadius: '8%',
+                        padding: '5px 30px',
+                        color: 'white',
+                        border: 'none'
+                    }}
+                    onClick={toggleCounter}
+                >
+                    {isRunning ? 'Stop' : 'Start'}
+                </button>
+                <button
+                    style={{
+                        fontSize: '60%',
+                        position: 'relative',
+                        top: '20vh',
+                        marginLeft: '5px',
+                        backgroundColor: 'red',
+                        borderRadius: '8%',
+                        padding: '5px 30px',
+                        color: 'white',
+                        border: 'none'
+                    }}
+                    onClick={resetCounters}
+                >
+                    Reset
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Grid;
